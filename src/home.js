f27782d9c77df3d100e06b2811cdc6a6881fb533
Ahmed safwat <a01273477051@gmail.com>
2023-12-25 12:04:55 +0200

    useeffect

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
@@ -17,7 +17,9 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
-
+  useEffect(() => {
+    console.log(blogs);
+  }, [blogs]);
   return (
     <div className="home">
       <BlogList blogs={blogs} title="all blogs" handleDelete={handleDelete} />
