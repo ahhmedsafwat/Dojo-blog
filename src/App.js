0492f2ae45ac8e802c1eaa3cb4da11b249a8714f
Ahmed safwat <a01273477051@gmail.com>
2023-12-29 20:34:51 +0200

    done with this project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./navbar";
 import Home from "./home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./create";
+import BlogDetails from "./blogdetails";
+import NotFound from "./notfound";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/usefetch.js
+++ src/usefetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("could not fetch the data for that resource");
